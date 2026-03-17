926f26f0a4f8b982cc88914b02ce003e88314a8c
lorriant <brearey88@yandex.ru>
2026-03-17 10:04:16 +0900

    all servo begin

--- wrestling-robot/wrestling-robot.ino
+++ wrestling-robot/wrestling-robot.ino
@@ -3,26 +3,36 @@ SoftwareSerial mySerial(3, 2); // RX, TX
 
 // servo
 #include <Servo.h>
-Servo myservo;
-#define SERVO_PIN 5
-#define SERVO_VCC 12
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;
+Servo myservo6;
+
+#define SERVO_1 3
+#define SERVO_2 5
+#define SERVO_3 6
+#define SERVO_4 9
+#define SERVO_5 10
+#define SERVO_6 11
+
+#define BLE_VCC 12
 
 void print(String msg) {
   mySerial.println(msg);
   Serial.println(msg);
 }
 
-void servoTest() {
-  myservo.write(0); // degrees
-  delay(200);
-  myservo.write(180); // degrees
-  delay(200);
-}
-
 void servoInit() {
-  pinMode(SERVO_VCC, OUTPUT);
-  digitalWrite(SERVO_VCC, HIGH);
-  myservo.attach(SERVO_PIN);
+  pinMode(BLE_VCC, OUTPUT);
+  digitalWrite(BLE_VCC, HIGH);
+  myservo1.attach(SERVO_1);
+  myservo2.attach(SERVO_2);
+  myservo3.attach(SERVO_3);
+  myservo4.attach(SERVO_4);
+  myservo5.attach(SERVO_5);
+  myservo6.attach(SERVO_6);
   // servoTest();
 }
 
@@ -41,16 +51,34 @@ void loop() {
     switch (cmd) {
       case 'f':
           print("Forward");
-          myservo.write(0); // degrees
+          myservo1.write(0); // degrees
+          myservo2.write(0); // degrees
+          myservo3.write(0); // degrees
+          myservo4.write(0); // degrees
+          myservo5.write(0); // degrees
+          myservo6.write(0); // degrees
+          print(String(myservo1.read()));
+          print(String(myservo2.read()));
+          print(String(myservo3.read()));
+          print(String(myservo4.read()));
+          print(String(myservo5.read()));
+          print(String(myservo6.read()));
           break;
 
       case 'b':
           print("Backward");
-          myservo.write(180); // degrees
-          break;
-      case 's':
-          print("Servo");
-          print(String(myservo.read()));
+          myservo1.write(180); // degrees
+          myservo2.write(180); // degrees
+          myservo3.write(180); // degrees
+          myservo4.write(180); // degrees
+          myservo5.write(180); // degrees
+          myservo6.write(180); // degrees
+          print(String(myservo1.read()));
+          print(String(myservo2.read()));
+          print(String(myservo3.read()));
+          print(String(myservo4.read()));
+          print(String(myservo5.read()));
+          print(String(myservo6.read()));
           break;
     }
   }
