5e14e2a34ac6301397241bdef00178e7f46d168c
lorriant <brearey88@yandex.ru>
2026-05-22 11:16:34 +0900

    fix JS and deleted nonuse video

--- course/landing_page/chatgpt/script.js
+++ course/landing_page/chatgpt/script.js
@@ -27,15 +27,3 @@ document.querySelectorAll(
   el.classList.add("fade-in");
   observer.observe(el);
 });
-
-const form = document.getElementById("registerForm");
-
-form.addEventListener("submit", event => {
-  event.preventDefault();
-
-  alert(
-    "Спасибо! Ваша заявка отправлена. Мы свяжемся с вами в ближайшее время."
-  );
-
-  form.reset();
-});
