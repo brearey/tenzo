264bf11cacc14f696b10071185deb2c02f436144
lorriant <brearey88@yandex.ru>
2026-05-30 10:01:52 +0900

    hello done with 8 servo

--- wrestling-test/wrestling-test.ino
+++ wrestling-test/wrestling-test.ino
@@ -1,22 +1,23 @@
 #include <VarSpeedServo.h>
 
 // Создаем объекты сервоприводов
-VarSpeedServo myservo4; // правая стопа
-VarSpeedServo myservo5; // правое колено
-VarSpeedServo myservo6; // правое бедро
-VarSpeedServo myservo7; // правая рука
+VarSpeedServo myservo4;   // правая стопа
+VarSpeedServo myservo5;   // правое колено
+VarSpeedServo myservo6;   // правое бедро
 
-VarSpeedServo myservo8; // левая рука
-VarSpeedServo myservo9; // левое бедро
-VarSpeedServo myservo10; // левое колено
-VarSpeedServo myservo11; // левая стопа
+VarSpeedServo myservo7;   // правая рука
+VarSpeedServo myservo8;   // левая рука
+
+VarSpeedServo myservo9;   // левое бедро
+VarSpeedServo myservo10;  // левое колено
+VarSpeedServo myservo11;  // левая стопа
 
 // Настройки
 #define LEG_SPEED 30      // 0=full speed, 1-255 slower to faster
 #define HAND_SPEED 80      // 0=full speed, 1-255 slower to faster
 #define IS_WAIT false // false = асинхронно (не блокирует код), true = ждать окончания движения
 
-// Пины сервоприводов согласно wrestling-robot.ino
+// Пины
 #define SERVO_4 4
 #define SERVO_5 5
 #define SERVO_6 6
@@ -26,18 +27,20 @@ VarSpeedServo myservo11; // левая стопа
 #define SERVO_10 10
 #define SERVO_11 11
 
-#define S4_START 90
+// Стартовые положения
+#define S4_START 85
 #define S5_START 90
-#define S6_START 90
+#define S6_START 95
 #define S7_START 90
 #define S8_START 90
-#define S9_START 90
-#define S10_START 90
-#define S11_START 90
+#define S9_START 95
+#define S10_START 82
+#define S11_START 97
 
-// Диапазоны импульсов (мкс) согласно вашему предыдущему коду
+// Диапазоны импульсов
 #define FEETCH_MIN 544
 #define FEETCH_MAX 2400
+
 #define TOWER_MIN 500
 #define TOWER_MAX 2500
 
@@ -45,106 +48,144 @@ void print(String msg) {
   Serial.println(msg);
 }
 
-void toStartAll() {
-  // обе ноги на исходную
-  moveLeg(myservo4, S4_START);
-  moveLeg(myservo5, S5_START);
-  moveLeg(myservo6, S6_START);
-  moveLeg(myservo7, S7_START);
-  moveLeg(myservo8, S8_START);
-  moveLeg(myservo9, S9_START);
-  moveLeg(myservo10, S10_START);
-  moveLeg(myservo11, S11_START);
-  delay(500);
-}
-
-void servoInit() {
-  // Инициализация с индивидуальными диапазонами
-  myservo1.attach(SERVO_1, TOWER_MIN, TOWER_MAX);
-  myservo2.attach(SERVO_2, TOWER_MIN, TOWER_MAX);
-  myservo3.attach(SERVO_3, FEETCH_MIN, FEETCH_MAX);
-  myservo4.attach(SERVO_4, TOWER_MIN, TOWER_MAX);
-  myservo5.attach(SERVO_5, FEETCH_MIN, FEETCH_MAX);
-  myservo6.attach(SERVO_6, FEETCH_MIN, FEETCH_MAX);
-}
-
-void moveLeg(VarSpeedServo s, int angle) {
+void moveLeg(VarSpeedServo &s, int angle) {
   s.write(angle, LEG_SPEED, IS_WAIT);
 }
 
-void moveHand(VarSpeedServo s, int angle) {
+void moveHand(VarSpeedServo &s, int angle) {
   s.write(angle, HAND_SPEED, IS_WAIT);
 }
 
+void servoInit() {
+
+  myservo4.attach(SERVO_4, FEETCH_MIN, FEETCH_MAX);
+  myservo5.attach(SERVO_5, FEETCH_MIN, FEETCH_MAX);
+  myservo6.attach(SERVO_6, FEETCH_MIN, FEETCH_MAX);
+
+  myservo7.attach(SERVO_7, TOWER_MIN, TOWER_MAX);
+  myservo8.attach(SERVO_8, TOWER_MIN, TOWER_MAX);
+
+  myservo9.attach(SERVO_9, FEETCH_MIN, FEETCH_MAX);
+  myservo10.attach(SERVO_10, FEETCH_MIN, FEETCH_MAX);
+  myservo11.attach(SERVO_11, FEETCH_MIN, FEETCH_MAX);
+}
+
+void toStartAll() {
+
+  moveLeg(myservo4, S4_START);
+  moveLeg(myservo5, S5_START);
+  moveLeg(myservo6, S6_START);
+
+  moveHand(myservo7, S7_START);
+  moveHand(myservo8, S8_START);
+
+  moveLeg(myservo9, S9_START);
+  moveLeg(myservo10, S10_START);
+  moveLeg(myservo11, S11_START);
+
+  delay(500);
+}
+
 void setup() {
-  print("Program start");
+
   Serial.begin(9600);
+  print("Program start");
+
   servoInit();
   toStartAll();
 
-  int leg_shift = 15;
+  int leg_shift = 10;
   int hand_shift = 90;
   int stage_delay = 500;
 
   // Hello
-  moveHand(myservo4, S4_START - hand_shift); // left hand up
+
+  moveHand(myservo8, S8_START - hand_shift);
   delay(1000);
-  moveHand(myservo4, S4_START); // left hand to start
+
+  moveHand(myservo8, S8_START);
   delay(stage_delay);
 
-  // Move hands
+  // Танец руками
+
   hand_shift = 30;
+
   for (int i = 0; i < 2; i++) {
-    moveHand(myservo3, S3_START + hand_shift); // right hand UP
-    moveHand(myservo4, S4_START + hand_shift); // left hand DOWN
+
+    moveHand(myservo7, S7_START + hand_shift);
+    moveHand(myservo8, S8_START + hand_shift);
+
     delay(400);
-    moveHand(myservo3, S3_START - hand_shift); // right hand DOWN
-    moveHand(myservo4, S4_START - hand_shift); // left hand UP
+
+    moveHand(myservo7, S7_START - hand_shift);
+    moveHand(myservo8, S8_START - hand_shift);
+
     delay(400);
   }
-  moveHand(myservo3, S3_START); // right hand to start
-  moveHand(myservo4, S4_START); // left hand to start
+
+  moveHand(myservo7, S7_START);
+  moveHand(myservo8, S8_START);
+
   delay(stage_delay);
 
-  // Move left leg
+  // Поднять левую ногу
+
   hand_shift = 90;
-  moveHand(myservo4, S4_START - hand_shift); // left hand up
-  moveHand(myservo3, S3_START + hand_shift); // right hand up
+
+  moveHand(myservo8, S8_START - hand_shift);
+  moveHand(myservo7, S7_START + hand_shift);
+
+  moveLeg(myservo10, S10_START - leg_shift);
+  moveLeg(myservo11, S11_START + leg_shift);
+
+  delay(1000);
+
+  moveHand(myservo8, S8_START);
+  moveHand(myservo7, S7_START);
+
+  moveLeg(myservo10, S10_START);
+  moveLeg(myservo11, S11_START);
+
+  delay(stage_delay);
+
+  // Поднять правую ногу
+
+  moveHand(myservo8, S8_START - hand_shift);
+  moveHand(myservo7, S7_START + hand_shift);
+
   moveLeg(myservo5, S5_START - leg_shift);
-  moveLeg(myservo6, S6_START + leg_shift);
+  moveLeg(myservo4, S4_START + leg_shift);
+
   delay(1000);
-  moveHand(myservo4, S4_START);
-  moveHand(myservo3, S3_START);
+
+  moveHand(myservo8, S8_START);
+  moveHand(myservo7, S7_START);
+
   moveLeg(myservo5, S5_START);
-  moveLeg(myservo6, S6_START);
+  moveLeg(myservo4, S4_START);
+
   delay(stage_delay);
 
-  // Move right leg
-  moveHand(myservo4, S4_START - hand_shift); // left hand up
-  moveHand(myservo3, S3_START + hand_shift); // right hand up
-  moveLeg(myservo1, S1_START - leg_shift);
-  moveLeg(myservo2, S2_START + leg_shift);
-  delay(1000);
-  moveHand(myservo4, S4_START);
-  moveHand(myservo3, S3_START);
-  moveLeg(myservo1, S1_START);
-  moveLeg(myservo2, S2_START);
-  delay(stage_delay);
+  // Bye Bye
 
-  // Bye bye
-  moveHand(myservo4, 0); // left hand up
+  moveHand(myservo8, 0);
+
   delay(1000);
+
   for (int i = 0; i < 2; i++) {
-    moveHand(myservo4, 10); // left hand DOWN
+
+    moveHand(myservo8, 10);
     delay(100);
-    moveHand(myservo4, 0); // left hand UP
+
+    moveHand(myservo8, 0);
     delay(100);
   }
-  
+
   delay(1000);
-  moveHand(myservo4, S4_START);
+
+  moveHand(myservo8, S8_START);
 }
 
 void loop() {
-  
+
 }
